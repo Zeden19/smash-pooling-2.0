01d167a024d3c691d94dd670a7ed993cbbe58278
arjunsharma <ashar562@uwo.ca>
2024-09-05 21:11:34 -0400

    Get markers working

--- app/components/GoogleMap.tsx
+++ app/components/GoogleMap.tsx
@@ -1,5 +1,6 @@
 "use client";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { AdvancedMarker, APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
+import { useState } from "react";
 
 //Map's styling
 export const defaultMapContainerStyle = {
@@ -12,19 +13,29 @@ const mapCentre = {
   lat: 43.6532,
   lng: -79.3832,
 };
-const zoom = 18;
-
-const mapOptions = {
-  zoomControl: true,
-  tilt: 0,
-  gestureHandling: "auto",
-};
+const zoom = 6;
 
 function GoogleMap() {
+  const [markers, setMarkers] = useState<{ lat: number; lng: number }[]>([
+    { lat: 53.54992, lng: 10.00678 },
+  ]);
   return (
     <div style={defaultMapContainerStyle}>
+      <button onClick={() => setMarkers([...markers, { lat: 43.6532, lng: -79.3832 }])}>
+        Add marker to Toronto
+      </button>
       <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string}>
-        <Map defaultZoom={zoom} defaultCenter={mapCentre}></Map>
+        <Map
+          style={{ width: "100%", height: "100%" }}
+          gestureHandling={"greedy"}
+          defaultZoom={zoom}
+          defaultCenter={mapCentre}
+          mapId={process.env.NEXT_PUBLIC_MAP_ID as string}
+        />
+
+        {markers.map((marker, index) => (
+          <AdvancedMarker key={index} position={marker} />
+        ))}
       </APIProvider>
     </div>
   );
